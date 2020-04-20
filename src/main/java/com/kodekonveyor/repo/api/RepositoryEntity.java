e9f2b9f81c513a0251636b00a090c7d308cd57da
murug <murug@DESKTOP-U5DR24L>
2020-04-20 14:30:27 +0530

    repository entity

--- src/main/java/com/kodekonveyor/repo/api/RepositoryEntity.java
+++ src/main/java/com/kodekonveyor/repo/api/RepositoryEntity.java
@@ -1,13 +1,20 @@
 package com.kodekonveyor.repo.api;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.Data;
 
 @Data
 @Entity
-public class RepositoryEntity {
-  private String url;
-  private ElementEntity owner;
 
+public class RepositoryEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+  private ElementEntity owner;
+  private String url;
 }
