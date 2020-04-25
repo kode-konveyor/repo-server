0a40439ce34180f2da11c06da697d9dbc5d85f79
Mayank Raj <mayankraj498@gmail.com>
2020-04-25 18:19:05 +0530

    trying to fix - recipe for target 'pmdcheck' failed issue.

--- src/test/java/com/kodekonveyor/repo/api/ModifyElementControllerElementValidationTest.java
+++ src/test/java/com/kodekonveyor/repo/api/ModifyElementControllerElementValidationTest.java
@@ -24,45 +24,44 @@ public class ModifyElementControllerElementValidationTest
 extends ModifyElementControllerTestBase {
 
   @Test
-  @DisplayName(value = "found local should be accepted")
+  @DisplayName("found local should be accepted")
   public void test1() {
 
     ThrowableTester.assertNoException(()->modifyElementController.call(ModificationDTOTestData.get(),"foundLocalRepoName","foundLocalTagName"));
   }
   @Test
-  @DisplayName(value = "syntax should not be accepted")
+  @DisplayName("syntax should not be accepted")
   public void test2() {
 
     ThrowableTester.assertThrows(()->modifyElementController.call(ModificationDTOTestData.getWithWrongSyntax(),"syntaxRepoName","syntaxTagName")).assertException(ConstraintException.class);
   }
 
   @Test
-  @DisplayName(value = "found server should be accepted")
+  @DisplayName("found server should be accepted")
   public void test3() {
 
     ThrowableTester.assertNoException(()->modifyElementController.call(ModificationDTOTestData.getFoundServer(),"foundServerRepoName","foundServerTagName"));
   }
 
   @Test
-  @DisplayName(value = "remote should be accepted")
+  @DisplayName("remote should be accepted")
   public void test4() {
 
     ThrowableTester.assertNoException(()->modifyElementController.call(ModificationDTOTestData.getRemote(),"remoteRepoName","remoteTagName"));
   }
 
   @Test
-  @DisplayName(value = "not found local should not be accepted")
+  @DisplayName("not found local should not be accepted")
   public void test5() {
 
     ThrowableTester.assertThrows(()->modifyElementController.call(ModificationDTOTestData.getNotFoundLocal(),"notFoundLocalRepoName","notFoundLocalTagName")).assertException(ConstraintException.class);
   }
 
   @Test
-  @DisplayName(value = "not found server should not be accepted")
+  @DisplayName("not found server should not be accepted")
   public void test6() {
 
     ThrowableTester.assertThrows(()->modifyElementController.call(ModificationDTOTestData.getNotFoundServer(),"notFoundServerRepoName","notFoundServerTagName")).assertException(ConstraintException.class);
-
   }
 
 }
