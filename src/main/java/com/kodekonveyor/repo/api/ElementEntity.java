d818e3487533ee909da952c8b59cce4227c488c4
murug <murug@DESKTOP-U5DR24L>
2020-04-30 20:25:18 +0530

    adding missing onetoone annotation

--- src/main/java/com/kodekonveyor/repo/api/ElementEntity.java
+++ src/main/java/com/kodekonveyor/repo/api/ElementEntity.java
@@ -1,9 +1,11 @@
 package com.kodekonveyor.repo.api;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 import lombok.Data;
 
@@ -20,7 +22,7 @@ class ElementEntity {
   private String id;
 
   private String name;
-
+  @OneToOne(fetch = FetchType.LAZY)
   private RepositoryEntity repo;
 
   private String sourceId;
