21e804f33394394ee375f38a6e4eaf52ff6c634d
Firdous18 <Muneer Mohammed@LAPTOP-K8KLOCU5>
2020-07-19 23:06:18 +0530

    Removed unnecessary code

--- src/main/java/com/kodekonveyor/repo/api/RepositoryEntityRepository.java
+++ src/main/java/com/kodekonveyor/repo/api/RepositoryEntityRepository.java
@@ -7,10 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface RepositoryEntityRepository
     extends CrudRepository<RepositoryEntity, Long> {
 
-  //List<RepositoryEntity> findByName(ElementEntity elementEntity);
-
-  List<RepositoryEntity> findByName(String repoName);
-
   List<RepositoryEntity> findByOwner(ElementEntity elementEntity);
 
 }
